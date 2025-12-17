c839c3eafd6a95a4aeb43d06c4c7a16098ea9d84
mirnanodes <formeandgame78@gmail.com>
2025-12-17 16:17:45 +0700

    feat: Notifikasi pada menu pekerjaan

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -129,5 +129,30 @@
     });
 </script>
 @stack('js')
+
+{{-- Notifikasi Pop-up - Mirna --}}
+@if(session('success'))
+<div id="toast-notification" class="fixed top-5 right-5 z-50 flex items-center w-full max-w-xs p-4 text-white bg-green-600 rounded-lg shadow-lg">
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-200 bg-green-700 rounded-lg">
+        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+        </svg>
+    </div>
+    <div class="ml-3 text-sm font-medium">{{ session('success') }}</div>
+    <button type="button" onclick="closeToast()" class="ml-auto -mx-1.5 -my-1.5 text-white hover:text-gray-200 rounded-lg p-1.5 inline-flex h-8 w-8">
+        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+        </svg>
+    </button>
+</div>
+<script>
+    function closeToast() {
+        document.getElementById('toast-notification').remove();
+    }
+    setTimeout(closeToast, 3000);
+</script>
+@endif
+{{-- End Notifikasi Pop-up - Mirna --}}
+
 </body>
 </html>
